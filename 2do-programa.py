e93b1e84c7b2762857c56fbe4a0980042647a148
Dylan-Esquivias <Dylan.esquivias@alumnos.udg.mx>
2023-06-02 15:46:33 -0600

    hice algunos archivos más sobre cantidad y precio

--- 2do-programa.py
+++ 2do-programa.py
@@ -1,4 +1,4 @@
 precioDeManzana :int = 10 #el gato se utiliza para comentarios 
-cantidadDeManzanas :int = 5
+cantidadDeManzanas :int = 5 #el int es solo para especificar a otros desarrolladores que de preferencia ingresen enteros
 print("Vas a pagar")
 print(precioDeManzana * cantidadDeManzanas)
---
+++ 4to-programa.py
@@ -0,0 +1,10 @@
+
+precio : int = int(input("Ingresa el precio de las manzanas: ")) # al colocar int() señalamos que lo que vamos a ingresar son números
+cantidad : int = int(input("Ingresa la cantidad de manzanas: "))
+print("las manzanas están en: " +str (precio))
+print("las manzanas verdes están en: " , precio)
+print(precio)
+print("la cantidad es: ")
+print(cantidad)
+print("Vas a pagar: ")
+print(precio * cantidad)
